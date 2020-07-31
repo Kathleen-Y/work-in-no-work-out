fbc6bc1a08bba8e6368fd1a136d27623963cd2b3
Kathleen Yoannon <k.yoannon@gmail.com>
2020-07-31 14:40:05 -0700

    Create server.js
    consts

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+// Dependencies
+
+const express        = require('express');
+const mongoose 			 = require('mongoose');
+
+const PORT = process.env.PORT || 5000;
+
+// Instantiate our app
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
