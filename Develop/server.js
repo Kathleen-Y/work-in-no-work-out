e17c634e0e6c82f18cf6a8d7698a31cb3d5de148
Kathleen Yoannon <k.yoannon@gmail.com>
2020-08-03 11:48:21 -0700

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,9 @@
 // Dependencies
-<<<<<<< HEAD
-// ============
+
 const express        = require('express');
 const mongoose 			 = require('mongoose');
 
 const PORT = process.env.PORT || 3000;
-=======
-
-const express        = require('express');
-const mongoose 			 = require('mongoose');
-
-const PORT = process.env.PORT || 5000;
->>>>>>> 9696f95fe008ae8e7374b0c677b00fe5fab873bc
 
 // Instantiate our app
 const app = express();
@@ -21,31 +13,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-<<<<<<< HEAD
+// mongo workout
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-=======
+  
+// mongo buget
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
->>>>>>> 9696f95fe008ae8e7374b0c677b00fe5fab873bc
+
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-<<<<<<< HEAD
 // routes
-app.use(require("./routes/apiRoutes"));
-app.use(require("./routes/htmlRoutes"));
+app.use(require("./routes/apiRoute"));
+app.use(require("./routes/htmlRoute"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
 
-
 module.exports = mongoose.connection
-=======
-
-
-
-
-
-module.exports = mongoose.connection
->>>>>>> 9696f95fe008ae8e7374b0c677b00fe5fab873bc
