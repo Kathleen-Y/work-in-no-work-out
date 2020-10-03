7ae62acd6b739fa82da09f383e69c32bd628b44b
Kathleen Yoannon <k.yoannon@gmail.com>
2020-10-03 13:44:26 -0700

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Tracker", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
